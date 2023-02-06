765be59ca67403d0d663100925dcda3102c554c6
Keevu1 <Kristjan827@gmail.com>
2023-02-06 12:00:51 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//See on kommentaar
+//rakendus küsib kasutajalt tema nime
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your neme: ");
+string userName = Console.ReadLine();
+Console.WriteLine("hello, "+userName+"!");
+
+//string interpolation
+Console.WriteLine($"hello, {userName}!");
